0cbfd74ab5b11cb5d9994bcf2f23fe2b943baa6a
Oleg Palashynskyi <106759871+0Palah@users.noreply.github.com>
2023-01-25 10:23:19 +0200

    3

---
+++ transactions/transaction.model.js
@@ -0,0 +1,72 @@
+const { Schema, model, SchemaTypes } = require("mongoose");
+
+const transactionSchema = new Schema(
+  {
+    athor: {
+      type: String,
+    },
+    creator: {
+      type: String,
+    },
+    auditor: {
+      type: String,
+    },
+    transactionDate: {
+      type: String,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense", "transfer"],
+    },
+    countId: {
+      type: String,
+    },
+    subCountId: {
+      type: String,
+    },
+    transferCountId: {
+      type: String,
+    },
+    transferSubCountIdTo: {
+      type: String,
+    },
+    categoryId: {
+      type: String,
+    },
+    subCategoryId: {
+      type: String,
+    },
+    document: {
+      type: String,
+    },
+    project: {
+      type: String,
+    },
+    customer: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+    },
+    status: {
+      type: String,
+    },
+    mark: {
+      type: String,
+    },
+    tags: {
+      type: String,
+    },
+    comment: {
+      type: String,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const TransactionModel = model("transaction", transactionSchema);
+
+module.exports = TransactionModel;
