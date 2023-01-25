e2ba9becfa49309f5e430b1e4b882d1dc42af24e
Oleg Palashynskyi <106759871+0Palah@users.noreply.github.com>
2023-01-25 14:54:20 +0200

    +refresh

--- users/users.service.js
+++ users/users.service.js
@@ -1,10 +1,16 @@
 const UserModel = require("./user.model");
 
 const findUserById = async (id) => {
-  const result = await UserModel.findById(id);
-  return result;
+  return await UserModel.findById(id);
+};
+
+const findUserByIdAndUpdate = async (id, updateData) => {
+  return await UserModel.findByIdAndUpdate(id, updateData, {
+    new: true,
+  });
 };
 
 module.exports = {
   findUserById,
+  findUserByIdAndUpdate,
 };
--- users/usersControllers/refreshToken.js
+++ users/usersControllers/refreshToken.js
@@ -1,16 +1,17 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const UsersService = require("../users.service");
 // const cookieParser = require("cookie-parser");
 
 const { createError } = require("../../helpers/createError");
-const User = require("../../models/users");
+// const User = require("../../models/users");
 
 const { JWT_SECRET_KEY, JWT_REFRESH_SECRET_KEY } = process.env;
 
 async function refreshToken(req, res) {
   const { id } = req.user;
 
-  const user = await User.findById(id);
+  const user = await UsersService.findUserById(id);
 
   if (!user) {
     throw createError({ status: 401, message: "Not authorized" });
@@ -25,11 +26,10 @@ async function refreshToken(req, res) {
     expiresIn: "30d",
   });
 
-  const userData = await User.findByIdAndUpdate(
-    user.id,
-    { token, refreshToken },
-    { new: true }
-  );
+  const userData = await UsersService.findUserByIdAndUpdate(user.id, {
+    token,
+    refreshToken,
+  });
 
   res.cookie("refreshToken", refreshToken, {
     maxAge: 30 * 24 * 60 * 60 * 1000,
